d886ed177b61089c498cba90c67d32f7b13006c3
Kimeunhye95 <49854766+Kimeunhye95@users.noreply.github.com>
2019-07-27 21:34:27 +0900

    입소신청 분양신청 관리자페이지 마이페이지 내역띄우기 완료 신청내역 현황 변화 진행중

--- src/com/kh/semi/entrance/model/service/EntranceService.java
+++ src/com/kh/semi/entrance/model/service/EntranceService.java
@@ -78,10 +78,172 @@ public class EntranceService {
 		ArrayList<Entrance> entranceList = new EntranceDao().selectAllentranceApply(con, userNo, currentPage, limit);
 		ArrayList<ParcelOut> parceloutList = new EntranceDao().selectAllparceloutApply(con, userNo, currentPage2, limit2);
 
+		System.out.println("개인 서비스 : " + parceloutList);
+
 		list.put("entranceList", entranceList);
 		list.put("parceloutList", parceloutList);
 
 		return list;
+	}
+
+	public int getMyEntranceAllUserListCount() {
+		Connection con = getConnection();
+
+		int listCount = new EntranceDao().getMyEntranceAllUserListCount(con);
+
+		close(con);
+
+		return listCount;
+	}
+
+	public int getMyParceloutAllUserListCount() {
+		Connection con = getConnection();
+
+		int listCount = new EntranceDao().getMyParceloutAllUserListCount(con);
+
+		close(con);
+
+		return listCount;
+	}
+
+	public HashMap<String, Object> selectAllUserApply(int currentPage, int currentPage2, int limit, int limit2) {
+		Connection con = getConnection();
+		HashMap<String, Object> list = new HashMap<String, Object>();
+
+		ArrayList<Entrance> entranceList = new EntranceDao().selectAllUserEntranceApply(con, currentPage, limit);
+		ArrayList<ParcelOut> parceloutList = new EntranceDao().selectAllUserParceloutApply(con, currentPage2, limit2);
+
+		System.out.println("service 분양 : " + parceloutList);
+		System.out.println("service 입소 : " + entranceList);
+
+		list.put("entranceList", entranceList);
+		list.put("parceloutList", parceloutList);
+
+		close(con);
+
+		return list;
+	}
+
+	public int okEntranceApply(int entAppNo) {
+		Connection con = getConnection();
+
+		int result = new EntranceDao().okEntranceApply(con, entAppNo);
+
+		if(result > 0) {
+			commit(con);
+		}else {
+			rollback(con);
+		}
+
+		return result;
+	}
+
+	public int noEntranceApply(int entAppNo, String reason) {
+		Connection con = getConnection();
+
+		int result = new EntranceDao().noEntranceApply(con, entAppNo, reason);
+
+		if(result > 0) {
+			commit(con);
+		}else {
+			rollback(con);
+		}
+
+		return result;
+	}
+
+	public int okEntrance(int entAppNo) {
+		Connection con = getConnection();
+
+		int result = new EntranceDao().okEntrance(con, entAppNo);
+
+		if(result > 0) {
+			commit(con);
+		}else {
+			rollback(con);
+		}
+
+		return result;
+	}
+
+	public int noEntrance(int entAppNo, String reason) {
+		Connection con = getConnection();
+
+		int result = new EntranceDao().noEntrance(con, entAppNo, reason);
+
+		if(result > 0) {
+			commit(con);
+		}else {
+			rollback(con);
+		}
+
+		return result;
+	}
+
+	public EntranceDogInfo selectDogInfo(int entAppNo) {
+		Connection con = getConnection();
+
+		EntranceDogInfo dogInfo = new EntranceDao().selectDogInfo(con, entAppNo);
+
+		close(con);
+
+		return dogInfo;
+	}
+
+	public int okParceloutApply(int pcoAppNo) {
+		Connection con = getConnection();
+
+		int result = new EntranceDao().okParceloutApply(con, pcoAppNo);
+
+		if(result > 0) {
+			commit(con);
+		}else {
+			rollback(con);
+		}
+
+		return result;
+	}
+
+	public int noParceloutApply(int pcoAppNo, String reason) {
+		Connection con = getConnection();
+
+		int result = new EntranceDao().noParceloutApply(con, pcoAppNo, reason);
+
+		if(result > 0) {
+			commit(con);
+		}else {
+			rollback(con);
+		}
+
+		return result;
+	}
+
+	public int okParcelout(int pcoAppNo) {
+		Connection con = getConnection();
+
+		int result = new EntranceDao().okParcelout(con, pcoAppNo);
+
+		if(result > 0) {
+			commit(con);
+		}else {
+			rollback(con);
+		}
+
+		return result;
+	}
+
+	public int noParcelout(int pcoAppNo, String reason) {
+		Connection con = getConnection();
+
+		int result = new EntranceDao().noParcelout(con, pcoAppNo, reason);
+
+		if(result > 0) {
+			commit(con);
+		}else {
+			rollback(con);
+		}
+
+		return result;
 	}
 
 }
